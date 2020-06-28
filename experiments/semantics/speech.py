4edf21e3dd5889619938b8cbbcf63caa3b021981
TronTron <thankcreate@gmail.com>
2020-06-28 13:26:13 -0700

    all set

--- experiments/semantics/speech.py
+++ experiments/semantics/speech.py
@@ -56,7 +56,7 @@ def generateSpeechFile(inputText, identifier, api, voiceType):
 
     # Perform the text-to-speech request on the text input with the selected
     # voice parameters and audio file type
-    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
 
     # The response's audio_content is binary.
     if not path.exists(outputPathFolder):

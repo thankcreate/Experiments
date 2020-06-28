73fc4c87a6e3f43ab75af57e9dbedb96ceab76c0
TronTron <thankcreate@gmail.com>
2020-06-28 00:40:10 -0700

    disable_v2_behavior

--- experiments/semantics/semantic.py
+++ experiments/semantics/semantic.py
@@ -54,7 +54,8 @@
 # In[3]:
 
 
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior() 
 import tensorflow_hub as hub
 import matplotlib.pyplot as plt
 import numpy as np
